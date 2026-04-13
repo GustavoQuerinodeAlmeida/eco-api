be6090c73ff8934bd712ef4f5482de8799f9c073
O-Guiih <guiihsilvasousa1@gmail.com>
2026-04-13 16:29:32 -0300

    querino

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocs = require("./docs/swagger");
 const db = require("./config/db");
@@ -8,8 +8,10 @@ const app = express();
 const PORT = 3000;
 
 // --- MIDDLEWARES ---
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors({
+  origin: "http://localhost:5173" // 👈 seu Vue
+}));
+app.use(express.json());
 
 // --- IMPORTAÇÃO DE ROTAS ---
 const userRoutes = require("./routes/useRoutes");
@@ -17,7 +19,6 @@ const projectRoutes = require("./routes/projectRoutes");
 const postRoutes = require("./routes/postRoutes");
 const eventRoutes = require("./routes/eventRoutes");
 const newsRoutes = require("./routes/newsRoutes");
-// Novas rotas que acabamos de criar:
 const commentRoutes = require("./routes/commentRoutes");
 const likeRoutes = require("./routes/likeRoutes");
 const notificationRoutes = require("./routes/notificationRoutes");
@@ -25,23 +26,22 @@ const notificationRoutes = require("./routes/notificationRoutes");
 // --- SWAGGER ---
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// --- ROTAS DA API ---
+// --- ROTA TESTE ---
 app.get("/", (req, res) => {
   res.send("API EcoHub funcionando 🚀");
 });
 
-// Registro das rotas no Express
+// --- ROTAS DA API ---
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/news", newsRoutes);
-// Registro das novas rotas:
 app.use("/api/comments", commentRoutes);
 app.use("/api/likes", likeRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-// --- TESTE DE CONEXÃO COM O BANCO ---
+// --- TESTE DE BANCO ---
 app.get("/teste-banco", (req, res) => {
   db.query("SELECT 1", (err) => {
     if (err) {
@@ -51,8 +51,8 @@ app.get("/teste-banco", (req, res) => {
   });
 });
 
-// --- INICIALIZAÇÃO DO SERVIDOR ---
+// --- INICIAR SERVIDOR ---
 app.listen(PORT, () => {
-  console.log(`\n🚀 Servidor rodando em http://localhost:${PORT}`);
-  console.log(`📖 Documentação Swagger: http://localhost:${PORT}/api-docs`);
+  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+  console.log(`📖 Swagger: http://localhost:${PORT}/api-docs`);
 });
